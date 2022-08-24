64b78b73205298a67fb334075d0d54897c900539
leonardo.costa@csiway.com.br <lmaiacosta@gmail.com>
2022-08-24 19:15:43 -0300

    Acertado dentro do projeto LINKS de rotas, adicionado arquivos PRETTIER e ESLINT

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App(){
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/Cadastro" element={<Cadastro />} />
-                <Route path="/Contato" element={<Contato />} />
+                <Route path="/cadastro" element={<Cadastro />} />
+                <Route path="/contato" element={<Contato />} />
             </Routes>
         </BrowserRouter>
     )
